43e588f83d71baf38c13a75d23df22cbb9b7c5fe
Matt2939 <159350289+Matt2939@users.noreply.github.com>
2025-04-29 23:59:47 -0500

    Barrel Destruction

--- Kong Donkey/Assets/Scripts/Barrel.cs
+++ Kong Donkey/Assets/Scripts/Barrel.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
-    // Sound Variables
-    public List<AudioClip> breaking = new List<AudioClip>();
-    private AudioSource source;
-
     [Header("Barrel Settings")]
     public float fallChance = 0.2f;
     public float fallSpeed = 2f;    
@@ -15,11 +11,12 @@ public class Barrel : MonoBehaviour
     private bool isFalling = false; // Flag to check if the barrel is falling
 
     private Rigidbody2D rb;
+    private Animator anim;
 
     void Start(){
-        source = GetComponent<AudioSource>(); 
         
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         if (rb == null)
         {
             Debug.LogError("Rigidbody2D not found on barrel!");
@@ -47,22 +44,25 @@ public class Barrel : MonoBehaviour
                 Debug.Log("Barrel is falling!");
                 isFalling = true;
                 gameObject.layer = LayerMask.NameToLayer("Ladder");
+                anim.Play(stateName: "BarrelFall");
+                rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+                transform.rotation = Quaternion.Euler(0,0,0);
+
             }
         }
         else if(other.CompareTag("LadderBottomTrigger")){
             Debug.Log("Barrel has reached the bottom!");
             isFalling = false;
             gameObject.layer = LayerMask.NameToLayer("Objects"); // Reset the layer to default when it reaches the bottom
+            anim.Play(stateName: "BarrelRoll");
+            rb.constraints = RigidbodyConstraints2D.None;
+
         }
-    }
-
-    private void destroyBarrel() {
-        // Play breaking sound
-        AudioClip clip = breaking[Random.Range(0, breaking.Count)];
-        source.PlayOneShot(clip);
-
-        // Destroy the barrel
-        Destroy(gameObject);
+        else if(other.CompareTag("Fire"))
+        {
+            Debug.Log("Barrel reached the fire");
+            Destroy(this.gameObject);
+        }
     }
 
 
