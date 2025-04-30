db501a9c82ad6f141196a163f34a2bf70a5366ab
lucasply <lucasply2004@gmail.com>
2025-04-29 23:25:35 -0500

    Made a gamemanager to track lives

---
+++ Kong Donkey/Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Game Settings")]
+    public Player player; // Place player object here in the inspector
+    public int lives = 3;
+    public int score = 0;
+    public float respawnTime = 3f;
+
+    public GameObject lifeRemainingPrefab;
+    private List<GameObject> lifeIcons = new List<GameObject>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        InitializeLives();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void InitializeLives()
+    {
+        for (int i = 0; i < lives; i++)
+        {
+            var lifeIcon = Instantiate(lifeRemainingPrefab, Vector3.zero, Quaternion.identity, transform);
+            RectTransform rectTransform = lifeIcon.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2((i * 60 - 900), (rectTransform.anchoredPosition.y+1000));
+            lifeIcons.Add(lifeIcon);
+        }
+    }
+
+    private void RemoveLifeIcon(){
+        if (lifeIcons.Count > 0)
+        {
+            GameObject lifeIcon = lifeIcons[lifeIcons.Count - 1];
+            lifeIcons.RemoveAt(lifeIcons.Count - 1);
+            Destroy(lifeIcon);
+        }
+    }
+
+    public void playerDied(){
+        this.lives--;
+        RemoveLifeIcon();
+
+        if(lives <= 0){
+            Debug.Log("Game Over!");
+            // Implement game over logic here
+        }
+        else{
+            Debug.Log("Player died! Lives remaining: " + lives);
+            // Implement respawn logic here
+            Invoke(nameof(RespawnPlayer), respawnTime);
+        }
+    }
+
+    private void RespawnPlayer(){
+        // Implement respawn logic here
+        Debug.Log("Player respawned!");
+        this.player.gameObject.SetActive(true); // Make sure in player logic, to disable the player when it dies
+        this.player.transform.position = new Vector3(-4.597f, -4.151f, 0);
+
+    }
+}
