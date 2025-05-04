464e8ae2a9aa0cf76eb3d407e0d1406090db853c
Jonah <156850942+6332harlanj@users.noreply.github.com>
2025-05-04 16:52:26 -0500

    Added FX
    Added the sound effects and troubleshooted. Have yet to add the music

--- Kong Donkey/Assets/Scripts/Iggy.cs
+++ Kong Donkey/Assets/Scripts/Iggy.cs
@@ -7,21 +7,19 @@ public class Iggy : MonoBehaviour
     public List<AudioClip> iggySounds = new List<AudioClip>();
     private AudioSource source;
 
-    public float warningChance = 0.05f;
     private bool isPlaying = false;
 
     // Start is called before the first frame update
     void Awake()
     {
         source = GetComponent<AudioSource>();
-        //playIggySound(0);
+        playIggySound(0);
     }
 
     public void playIggySound(int index) {
         /*
             0 : help me
             1 : reaction to death
-            2 : be careful on ladder
         */
 
         AudioClip clip = iggySounds[index];
@@ -30,24 +28,10 @@ public class Iggy : MonoBehaviour
             Debug.Log("Sound clip not found at index: " + index);
             return;
         }
-        else if (isPlaying) {
+        else if (source.isPlaying) {
             return;
         }
-        else if (index == 2 && Random.value >= warningChance) {
-            return;
-        }
-
-        isPlaying = true;
 
         source.PlayOneShot(clip);
-        StartCoroutine(Wait(clip.length));
-        
-        isPlaying = false;
     }
-
-    IEnumerator Wait(float waitTime) {
-        yield return new WaitForSecondsRealtime(waitTime);
-    }
-
-
 }
